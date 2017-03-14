14bc2c120bbdcc52c7bef2b01291963aa67d6902
Cyborg_Ean <ean.director@gmail.com>
2017-03-14 14:20:13 -0700

    started on functions for controlling inputs

--- matrixTest.py
+++ matrixTest.py
@@ -16,12 +16,32 @@ plane.append(d5)
 def pretty(matrix):
     for i in matrix:
       print(i)
-      
+
 def xyAbstract(matrix,x,y,value):
     newMatrix = matrix
     matrix[y][x] = value
     return newMatrix
-    
+
+def controls(val):
+    action = ''
+    if val == "w":
+        action = "up"
+    elif val == "s":
+        action = "down"
+    elif val == "a":
+        action = "left"
+    elif val == "d":
+        action = "right"
+    return action
+
+def objAction(val):
+    if val = 
+
 #pretty(plane)
 plane = xyAbstract(plane,4,4,"Hey!")
 pretty(plane)
+
+running = True
+
+opt = input("Enter a character: ")
+print(opt)
