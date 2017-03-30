36bd4bf05b7297e00b243cb81730eaf4e6d5ebf2
Cyborg_Ean <ean.director@gmail.com>
2017-03-29 17:08:59 -0700

    experimenting with random test input

--- testModules.py
+++ testModules.py
@@ -1,18 +1,10 @@
 import unittest
+import random
 import modules.geometry as geo
 import modules.matrix as trix
 
-def square(x):
-    return x * x
+class geometrySlopeTest(unittest.TestCase):
 
-def opposite(x):
-    return not x
-
-class geometryTest(unittest.TestCase):
-
-    '''
-    Test Slopes!!
-    '''
     def test_slope_neg6_6(self):
         slope = geo.slope([8,2],[2,8])
         self.assertEqual([-6,6],slope,"Incorrect slope value")
@@ -28,6 +20,17 @@ class geometryTest(unittest.TestCase):
     def test_slope_negative_odds_133296(self):
         slope = geo.slope([-13,-3],[-29,-6])
         self.assertEqual([-16,-3],slope,"Incorrect slope value")
+
+    '''
+    def test_slope_random_values(self):
+        cap = 100
+        x1 = random.randint(-cap,cap)
+        y1 = random.randint(-cap,cap)
+        x2 = random.randint(-cap,cap)
+        y2 = random.randint(-cap,cap)
+        slope = geo.slope([x1,y1],[x2,y2])
+    '''
+
 
     def test_slope_data_type(self):
         slope = geo.slope([-4,9],[6,-5])
